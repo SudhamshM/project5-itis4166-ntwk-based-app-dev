c5b964cf2d536ef7c1466a4136e4c00a4c14adb3
Sudhamsh Mondrati <sudhamshmondrati@gmail.com>
2023-04-25 16:10:01 -0400

    Update input field requirements for event and user

--- models/meetupEvent.js
+++ models/meetupEvent.js
@@ -5,8 +5,8 @@ const meetupSchema = new Schema(
     {
         category: {type: String, enum: ['Action', 'Romance', 'Comedy', 'Horror', 'Other'], default: 'Other', required: [true, "Category is required"]},
         title: {type: String, required: [true, "Title is required"], minLength: [2, "Title should be at least 2 characters"], maxLength: [65, "Title should not exceed 50 characters."]},
-        details: {type: String, required: [true, "Details are required"], minLength: [10, "Description should be at least 10 characters"]},
-        location: {type: String, required: [true, "Location is required"], minLength: [3, "Location should be at least 3 characters"]},
+        details: {type: String, required: [true, "Details are required"], minLength: [10, "Description should be at least 10 characters"], maxLength: [100, "Description cannot exceed 100 characters."]},
+        location: {type: String, required: [true, "Location is required"], minLength: [3, "Location should be at least 3 characters"], maxLength: [60, "Location name cannot exceed 60 characters"]},
         hostName: {type: Schema.Types.ObjectId, ref: 'User'},
         startTime: {type: Date, required: [true, "Start date is required"]},
         endTime: {type: Date, required: [true, "End date is required"]},
